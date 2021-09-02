cff5498732406a2e3bc7ab8a9599815508d00864
Jack Ning <JackNing@jacks-mbp-2.lan>
2021-09-01 21:55:55 -0400

    minor edits

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Nav from "./Navigate";
 import Links from "./MediaLinks";
-import Routes from "./Routes";
+import Routes from "./routes";
 import { BrowserRouter as Router } from "react-router-dom";
 
 const App = () => {
